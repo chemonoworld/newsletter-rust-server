224cb898872c295620d908586ee238bc68ce8daf
Calvin Lee <jinwoo0451@naver.com>
2024-02-28 09:58:45 +0900

    feat: to 3.4

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,35 @@
-use actix_web::{web, App, HttpServer, Responder, HttpResponse, HttpRequest};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
 }
 
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
 
-#[tokio::main]
-async fn main() -> std::io::Result<()> {
+// #[tokio::main]
+async fn run() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
             .route("/{name}", web::get().to(greet))
     })
     .bind("localhost:8080")?
-        .run().await
+    .run()
+    .await
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::health_check;
+
+    #[tokio::test]
+    async fn health_check_succeeds() {
+        let response = health_check().await;
+        assert!(response.status().is_success())
+    }
+}
+
