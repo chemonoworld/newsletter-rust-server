dea17eff70b9b0b01e447555dbfe7fbf4a7cb1b4
Calvin Lee <jinwoo0451@naver.com>
2024-03-04 11:11:27 +0900

    fix: to 3.5

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,8 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use zero2prod::run;
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    run()?.await
 }
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
-
-// #[tokio::main]
-async fn run() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/health_check", web::get().to(health_check))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind("localhost:8080")?
-    .run()
-    .await
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::health_check;
-
-    #[tokio::test]
-    async fn health_check_succeeds() {
-        let response = health_check().await;
-        assert!(response.status().is_success())
-    }
-}
 
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,14 @@
-use zero2prod::main;
+// lib.rs 파일 함수 따로 use 없이 사용 가능
+// Cargo.toml [lib]에 작성하면 전역적? 사용가능한가봄..
 
-#[test]
-fn dummy_test() {
-    main()
+#[tokio::test]
+async fn health_check_works() {
+    spawn_app();
+}
+
+async fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to bind address");
+    // 아래서 await을 추가적으로 작성할 경우 지정된 주소를 무한히 대기함. 요청 도달시 처리하지만 스스로 종료 또는 완료하지 않음. 따라서 테스트 로직 실행 안됨
+    // tokio::spawn은 하나의 Future를 받아서 런타임에 전달해 등록하고 종료될 때까지 기다리지않음. 따라서 이후 Future 및 태스크(테스트 로직)과 동시에 실행 됨
+    let _ = tokio::spawn(server);
 }
